791b4253b94433b324b87463bdc7371d7e05508e
BramlyReed <ya.andreev99@ya.ru>
2020-11-16 23:14:32 -0800

    Initial commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Stas on 14.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
